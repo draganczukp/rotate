86aaf1be5f6e40c929aafd055b21631de7f0e9e1
Przemek Draganczuk <killermenpl@arch.home>
2017-03-14 15:25:28 +0100

    Updated

---
+++ android/src/pl/killermenpl/board/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package pl.killermenpl.board;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import pl.killermenpl.board.RotateGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RotateGame(), config);
+	}
+}
---
+++ desktop/src/pl/killermenpl/board/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.killermenpl.board.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.killermenpl.board.RotateGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RotateGame(), config);
+	}
+}
